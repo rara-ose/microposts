45245e45b60873d425ba652a1ada63116a7ed132
raraose <lovebyrara@icloud.com>
2016-12-17 13:19:13 +0000

    new5

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :location, allow_blank: true, length: { minimum: 2, maximum: 20 }
   has_secure_password
-  
+  has_many :microposts  
 end
