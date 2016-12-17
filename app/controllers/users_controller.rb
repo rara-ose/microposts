ae4199df2d0091455deb53dc4f75e3c5cc8feebc
raraose <lovebyrara@icloud.com>
2016-12-17 07:34:31 +0000

    提出

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :correct_user, only: [:edit, :update]
   def show # 追加
     @user = User.find(params[:id])
   end
@@ -22,20 +23,25 @@ class UsersController < ApplicationController
     end
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      # 更新に成功したときの処理
+      flash[:success] = 'Profile updated'
+      redirect_to @user
     else
       render 'edit'
     end
   end
 
+  
     
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
+    params.require(:user).permit(:name, :email, :location, :password,
                                  :password_confirmation)
-  end                               
+  end
+  def correct_user
+        @user = User.find(params[:id])
+    redirect_to root_path if @user != current_user
+  end
 end
