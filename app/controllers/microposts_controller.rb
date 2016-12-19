9f028015224179887fc7feb00836f432aaeb56fb
raraose <lovebyrara@icloud.com>
2016-12-19 23:11:29 +0000

    feed

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,6 +7,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
+      
       render 'static_pages/home'
     end
   end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,1 @@
-class StaticPagesController < ApplicationController
-  def home
-    @micropost = current_user.microposts.build if logged_in?
-  end
-end
+
